21d2010a1cfdfd91236e97f4e9e5b2602fe67699
yan.zan <yzan@DESKTOP-RI4HRUM.localdomain>
2018-09-03 21:37:17 -0700

    test conflict second times

--- test.java
+++ test.java
@@ -5,3 +5,4 @@ another line changes in local to test the first push
 test push force
 test the same file changes both in local and remote
 test the same file changes both in remote and local
+test conflicat again local
